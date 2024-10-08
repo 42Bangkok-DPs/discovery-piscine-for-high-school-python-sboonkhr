f46334e9954aec31eb306842099a89b12a395332
sboonkhr <sboonkhr@student.42bangkok.com>
2024-10-08 16:35:11 +0700

    Create to25.py

---
+++ cell03/to25.py
@@ -0,0 +1,11 @@
+n=int(input())
+count=25
+while(count>n):
+  n = n + 1
+  if(n == count):
+    print("End Program")
+  elif: n < count
+    continue
+    print("Inside The Loop, My Variable is",n)
+else:
+print("Error")
