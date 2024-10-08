b58577d7f796662e8e68daa7fb84a9036504b486
sboonkhr <sboonkhr@student.42bangkok.com>
2024-10-08 15:24:33 +0700

    Create mult.py

---
+++ cell02/mult.py
@@ -0,0 +1,12 @@
+x=int(input())
+y=int(input())
+z=x*y
+if z > 0:
+  print (x "x" y = z
+         "The result is positive")
+elif z < 0:
+  print (x "x" y = z
+         "The result is negative")
+else:
+  print (x "x" y = z
+         "The result is both positive and negative")
