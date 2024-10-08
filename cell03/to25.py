4f569fe21f79142defa4f788898d8bb257b51a11
sboonkhr <sboonkhr@student.42bangkok.com>
2024-10-08 16:42:45 +0700

    Update to25.py

--- cell03/to25.py
+++ cell03/to25.py
@@ -4,8 +4,5 @@ while(count>n):
   n = n + 1
   if(n == count):
     print("End Program")
-  elif: n < count
-    continue
-    print("Inside The Loop, My Variable is",n)
-else:
-print("Error")
+  else:
+    print("Error")
