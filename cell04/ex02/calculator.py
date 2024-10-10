ae398068df5ffc26cb196a02d054ac4ade117af1
sboonkhr <sboonkhr@student.42bangkok.com>
2024-10-10 12:53:06 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,7 @@
+first = int(input("Give me the first number : "))
+second = int(input("Give me the second number : "))
+plus = first + second
+minus = first - second
+mult = first*second
+divide = first/second
+print("Thank you !\n"f"{first}+{second}={plus}\n"f"{first}-{second}={minus}\n"f"{first}*{second}={mult}\n"f"{first}\{second}={divide}")
