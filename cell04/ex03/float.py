6176daddd9c1dbabf8de58677652236fec21a237
sboonkhr <sboonkhr@student.42bangkok.com>
2024-10-10 12:55:41 +0700

    Add files via upload

---
+++ cell04/ex03/float.py
@@ -0,0 +1,13 @@
+def czech(number):
+    if number.is_integer():
+        print("This number is an integer")
+    else:
+        print("This number is a demical")
+
+num = (input("Give me a number: "))
+
+try:
+    number= float(num)
+    czech(number)
+except ValueError:
+    print("Error")
