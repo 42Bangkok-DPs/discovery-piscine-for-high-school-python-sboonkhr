160e5ddff35f43b796dffc636dd651d4fb4a1c0d
sboonkhr <sboonkhr@student.42bangkok.com>
2024-10-10 13:57:30 +0700

    Add files via upload

---
+++ cell05/ex02/play_with_array++.py
@@ -0,0 +1,6 @@
+Original_array = [2, 8, 9, 48, 8, 22, -12, 2]
+New_array = [ ]
+for i in range(len(Original_array)):
+ New_array =  New_array + [Original_array[i] * 5]
+print("Original array" , Original_array)
+print("New array:", New_array)
