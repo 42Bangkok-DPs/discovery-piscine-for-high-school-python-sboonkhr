9c1ed875accf64540b8248ea7cff35378d33782f
sboonkhr <sboonkhr@student.42bangkok.com>
2024-10-11 14:49:30 +0700

    Update ages.py

--- cell04/ex01/ages.py
+++ cell04/ex01/ages.py
@@ -1,2 +1,3 @@
+#!/usr/bin/env python3
 f = int(input("Please tell me your age : "))
 print("You're currently {f} years old\n""In 10 years, You'll be",{f+10},"years old\n""In 20 years, You'll be",{f+20},"years old\n""In 30 years, You'll be",{f+30},"years old")
